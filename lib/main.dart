4ffd76bc311e87ca770817c4025741beb1813c51
Shan Peiris <spe@dipscloud.com>
2020-05-15 17:28:38 +0530

    implement first page and few UI fixes

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,53 @@ class NoteApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
+      appBar: AppBar(
+          title: Center(
+            child: Text(
+              "Note Hub",
+              style: TextStyle(fontSize: 30),
+            ),
+          ),
+          backgroundColor: Colors.yellow),
+      body: Login(),
+    ));
+  }
+}
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FloatingActionButton(
+          child: Text(
+            'Get Started',
+            style: TextStyle(fontSize: 10),
+          ),
+          //color: Colors.teal.shade100,
+          onPressed: () {
+            print('clicked');
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => SecondScreen()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
       appBar: AppBar(title: Text("Note Hub"), backgroundColor: Colors.yellow),
       body: UserNote(),
     ));
@@ -27,24 +74,28 @@ class UserNote extends StatefulWidget {
 class _UserNoteState extends State<UserNote> {
   TextEditingController nameController = TextEditingController();
   TextEditingController noteBodyController = TextEditingController();
-  List<Note> noteList = [];
-  var document;
+  bool _validateTitle = false;
   final CollectionReference noteCollection =
       Firestore.instance.collection('Note');
 
   void addItemToList() {
     print('called addItemToList');
-    //noteCollection.add(data)
-    String title=nameController.text;
-    String noteBody=noteBodyController.text;
-    var jsonString = '{"title": "$title", "note": "$noteBody"}';
-    Map<String, dynamic> note = jsonDecode(jsonString);
-    noteCollection.add(note);
+    String title = nameController.text.trim();
+    String noteBody = noteBodyController.text.trim();
+    if (title.length == 0) {
+      _validateTitle = true;
+    } else {
+      _validateTitle = false;
+      var jsonString = '{"title": "$title", "note": "$noteBody"}';
+      Map<String, dynamic> note = jsonDecode(jsonString);
+      noteCollection.add(note);
+    }
     print('finished addItemToList');
   }
 
   Widget _buidStream(BuildContext context, DocumentSnapshot document) {
     return ListTile(
+      contentPadding: EdgeInsets.fromLTRB(10, 2, 10, 2),
       title: Text(document['title']) ?? '',
       subtitle: Text(document['note']) ?? '',
     );
@@ -60,7 +111,8 @@ class _UserNoteState extends State<UserNote> {
             controller: nameController,
             decoration: InputDecoration(
               border: OutlineInputBorder(),
-              labelText: 'Note Name',
+              labelText: 'Note Title',
+              errorText: _validateTitle ? 'Title Can\'t Be Empty' : null,
             ),
           ),
         ),
@@ -69,20 +121,23 @@ class _UserNoteState extends State<UserNote> {
           child: TextField(
             controller: noteBodyController,
             decoration: InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Note',
-            ),
+                border: OutlineInputBorder(), labelText: 'Note'),
           ),
         ),
         Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            RaisedButton(
-              child: Text('Add'),
-              onPressed: () {
-                setState(() {
-                  addItemToList();
-                });
-              },
+            Container(
+              padding: EdgeInsets.all(10),
+              child: RaisedButton(
+                child: Text('Add'),
+                padding: EdgeInsets.all(10),
+                onPressed: () {
+                  setState(() {
+                    addItemToList();
+                  });
+                },
+              ),
             ),
           ],
         ),
@@ -92,14 +147,17 @@ class _UserNoteState extends State<UserNote> {
                 builder: (context, snapshot) {
                   if (!snapshot.hasData) return const Text('Loading');
 
-                  document = snapshot;
-                  return ListView.builder(
-                      padding: const EdgeInsets.all(8),
-                      itemCount: snapshot.data.documents.length,
-                      itemBuilder: (context, int index) {
-                        return _buidStream(
-                            context, snapshot.data.documents[index]);
-                      });
+                  return ListView.separated(
+                    separatorBuilder: (context, index) => Divider(
+                      color: Colors.black,
+                    ),
+                    padding: const EdgeInsets.all(8),
+                    itemCount: snapshot.data.documents.length,
+                    itemBuilder: (context, int index) {
+                      return _buidStream(
+                          context, snapshot.data.documents[index]);
+                    },
+                  );
                 }))
       ],
     );
